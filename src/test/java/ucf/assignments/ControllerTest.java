e024de7755f62820e5f7fce903e3083d13c68bfa
Monz <69062179+khyrdantai@users.noreply.github.com>
2021-07-01 13:17:37 -0400

    Added Controller and finished Part 1.

---
+++ src/test/java/ucf/assignments/ControllerTest.java
@@ -0,0 +1,68 @@
+/*
+ * UCF COP3330 Summer 2021 Assignment Solution
+ * Copyright 2021 Harry Hocker
+ */
+
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+	@Test
+	void clickAddList() {
+		// on user click, make console print out "list added"
+	}
+
+	@Test
+	void clickDeleteList() {
+		// on user click, make console print out "list deleted"
+	}
+
+	@Test
+	void clickShowCompleted() {
+		// on user click, make console print out "showing completed"
+	}
+
+	@Test
+	void clickHideCompleted() {
+		// on user click, make console print out "hiding completed"
+	}
+
+	@Test
+	void clickPlus() {
+		// on user click, make console print out "object added"
+	}
+
+	@Test
+	void clickMinus() {
+		// on user click, make console print out "object removed"
+	}
+
+	@Test
+	void menuLoadFile() {
+		// on user click, make console print out "file loaded"
+	}
+
+	@Test
+	void menuLoadList() {
+		// on user click, make console print out "list loaded"
+	}
+
+	@Test
+	void menuSaveFile() {
+		// on user click, make console print out "file saved"
+	}
+
+	@Test
+	void menuSaveList() {
+		// on user click, make console print out "list saved"
+	}
+
+	@Test
+	void menuQuit() {
+		// on user click, make console print out "program quit!"
+	}
+}
