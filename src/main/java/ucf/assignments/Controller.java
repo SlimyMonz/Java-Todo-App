90d691e52e4436416bf671df863890a4aa024386
Harry <69062179+khyrdantai@users.noreply.github.com>
2021-07-05 13:36:48 -0400

    Update Controller.java

--- src/main/java/ucf/assignments/Controller.java
+++ src/main/java/ucf/assignments/Controller.java
@@ -10,24 +10,28 @@ import javafx.event.ActionEvent;
 public class Controller {
 
 	// on user button press:
-
+	
+	@FXML
 	public void clickAddList(ActionEvent actionEvent) {
 		// run TodoListArray.addTodoList()
 	}
-
+	
+	@FXML
 	public void clickDeleteList(ActionEvent actionEvent) {
 		// run TodoListArray.delTodoList()
 	}
-
+	
+	@FXML
 	public void clickShowCompleted(ActionEvent actionEvent) {
 		// make objects that have completed=true to be visible in GUI
 	}
-
+	
+	@FXML
 	public void clickHideCompleted(ActionEvent actionEvent) {
 		// make objects that have completed=false invisible in GUI
 	}
 
-
+	@FXML
 	public void clickPlus(ActionEvent actionEvent) {
 		// figure out what list is already displaying info
 		// run Todolist.addTodo() to current displayed list
@@ -38,31 +42,33 @@ public class Controller {
 		// TodoList.delTodo(selected TodoItem)
 	}
 
-
+	@FXML
 	public void menuLoadFile(ActionEvent actionEvent) {
 		// run Path path = ManageFile.getFilePath()
 		// run ManageFile.loadFile(path)
 	}
 
+	@FXML
 	public void menuLoadList(ActionEvent actionEvent) {
 		// run Path path = ManageFile.getFilePath()
 		// run String name = ManageFile.getFileName()
 		// run ManageFile.loadList(path, name);
 	}
 
-
+	@FXML
 	public void menuSaveFile(ActionEvent actionEvent) {
 		// Path path = ManageFile.getFilePath()
 		// run ManageFile.saveFile(path)
 	}
 
+	@FXML
 	public void menuSaveList(ActionEvent actionEvent) {
 		// run Path path = ManageFile.getFilePath()
 		// run String name = ManageFile.getFileName()
 		// run ManageFile.saveList(path, name);
 	}
 
-
+	@FXML
 	public void menuQuit(ActionEvent actionEvent) {
 		// put code here that stops the app from running (don't yet know what that is)
 		// prompt to save before quitting? idk if I'm smart enough for that
